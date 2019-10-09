dfca3417ac3fd5f9104673245ec1a5f7a960d641
Yousef Lawrence <ylawrence@thacher.org>
2019-10-09 16:16:43 -0500

    forgot something

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { DatePicker } from "antd";
 
 function App() {
   return (
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+firebaseConfig = {
+  apiKey: "AIzaSyB6eW2oH4xQenxD014j5PaWMQC96eC9QQQ",
+  authDomain: "airspace-development-app.firebaseapp.com",
+  databaseURL: "https://airspace-development-app.firebaseio.com",
+  projectId: "airspace-development-app",
+  storageBucket: "airspace-development-app.appspot.com",
+  messagingSenderId: "109202295273",
+  appId: "1:109202295273:web:9cb8dade421f367f9da378",
+  measurementId: "G-8L0JLGSN9Y"
+};
+
+export default config;
